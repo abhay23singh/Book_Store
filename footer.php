ace39ee436f985482c7478469573619cac69a6eb
Abhay Pratap singh <abhay.singhchandel18@gmail.com>
2025-03-12 20:26:37 +0530

    changes in footer of home page

--- footer.php
+++ footer.php
@@ -36,6 +36,6 @@
 
    </div>
 
-   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>mr. web designer</span> </p>
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Abhay Pratap Singh</span> </p>
 
 </section>
